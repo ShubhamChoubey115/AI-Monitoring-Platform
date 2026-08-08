50c0cc62b9b9feaa419d23871cf987c56860a4ba
Shubham Choubey <shubhamchoubey115@gmail.com>
2026-08-08 21:57:33 +0530

    main class file

---
+++ services/discovery/src/test/java/com/ai_monitoring/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai_monitoring.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
